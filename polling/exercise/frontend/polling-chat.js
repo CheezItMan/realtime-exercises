fc082232ec063294f2523bf71537e468be780cbb
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-30 17:35:34 -0700

    backoff and retry

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -5,6 +5,8 @@ const msgs = document.getElementById("msgs");
 
 // let's store all current messages here
 let allChat = [];
+let failedTries = 0;
+const BACKOFF = 5000;
 
 // the interval to poll at in milliseconds
 const INTERVAL = 3000;
@@ -30,10 +32,10 @@ async function postNewMsg(user, text) {
     }
   };
   try {
-    allChat.unshift({
-      ...data
-    });
-    render();
+    // allChat.unshift({
+    //   ...data
+    // });
+    // render();
     const res = await fetch('/poll', options);
     const json = await res.json();
     console.log('Posted', json);
@@ -49,14 +51,22 @@ async function getNewMsgs() {
   try {
     const res = await fetch('/poll');
     json = await res.json();
+
+    // If there's an error
+    if (res.status >= 400) {
+      throw new Error("Request didn't succeed: " + res.status);
+    }
+    allChat = json.msg;
+    render();
+    failedTries = 0;
   } catch(error) {
     // Backoff code
     console.error('Polling Error', error);
-  }
 
-  allChat = json.msg;
-  render();
-  setTimeout(getNewMsgs, INTERVAL);
+    failedTries++;
+  }
+  //   removed for setAnimationFrame
+  // setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
@@ -73,4 +83,36 @@ const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
 // make the first request
-getNewMsgs();
+// getNewMsgs();
+
+// Much better because setTimeOut halts everything
+// including animation and drawing in the screen
+
+let timeToMakeNextRequest = 0;
+
+
+// Should be a bare-bones function because it gets called:
+//   A LOT!
+async function pollChat(time) {
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+    // Linear Backoff strategy
+    timeToMakeNextRequest = time + INTERVAL + BACKOFF * failedTries;
+  }
+
+  requestAnimationFrame(pollChat);
+}
+
+requestAnimationFrame(pollChat);
+
+
+/*
+Things to talk about:
+
+1.  SetInterval (why not do this?)
+2.  Try-catch & async await
+3.  What is the backoff strategy
+4.  What does requestAnimationFrame do?
+5.  DDOS  how is that a problem?  
+
+*/
