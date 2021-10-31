db3134c4cc855d32afbc66e8ec1d001406be65e9
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-30 17:01:34 -0700

    backend basic server

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+// Parses through the request body (not needed)
 import bodyParser from "body-parser";
+
+// Limited Array (length 50)
 import nanobuffer from "nanobuffer";
+
+// Logging Framework
 import morgan from "morgan";
 
+const BUFFER_SIZE = 50;
+
 // set up a limited array
-const msg = new nanobuffer(50);
+const msg = new nanobuffer(BUFFER_SIZE);
 const getMsgs = () => Array.from(msg).reverse();
 
 // feel free to take out, this just seeds the server with at least one message
@@ -17,17 +24,27 @@ msg.push({
 // get express ready to run
 const app = express();
 app.use(morgan("dev"));
+// Deprecated
 app.use(bodyParser.json());
 app.use(express.static("frontend"));
 
 app.get("/poll", function (req, res) {
-  // use getMsgs to get messages to send back
-  // write code here
+  res.json({
+    msg: getMsgs(),
+  });
 });
 
 app.post("/poll", function (req, res) {
-  // add a new message to the server
-  // write code here
+  const { user, text } = req.body;
+  msg.push({
+    user,
+    text,
+    time: Date.now(),
+  });
+  res.status(200).json({
+    status: "ok",
+    ok: true,
+  });
 });
 
 // start the server
